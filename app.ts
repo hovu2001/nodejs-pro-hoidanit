7849da0e3e0676256341fe742c5dad28f894e78f
hovu2001 <nguyenduyhoang26042002@gmail.com>
2025-10-17 20:33:20 +0700

    video 38

--- app.ts
+++ app.ts
@@ -1,10 +1,10 @@
-const express = require("express");
-
+// const express = require("express");
+import express from "express";
 const app = express();
 const PORT = 8080;
 
 app.get("/", (req, res)=>{
-   res.send("Hello word")
+   res.send("Hello world")
 })
 
 app.get("/hoidanit", (req, res)=>{
